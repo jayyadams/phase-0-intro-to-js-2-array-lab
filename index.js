0da14855fd7d3a1ccec99eca454e1f3818d4dd1a
Jordan Adams <jcadamsboomer@gmail.com>
2023-08-09 08:25:48 -0500

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+  }
+
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+  }
+
+  function appendCat(name) {
+    var newArray = cats.slice();
+    newArray.push(name)
+    return newArray;
+  }
+
+  function prependCat(name) {
+    var newArray = cats.slice();
+    newArray.unshift(name)
+    return newArray;
+  }
+
+  function removeLastCat() {
+    var peanut= cats.slice(0,cats.length-1);
+    return peanut;
+  }
+
+  function removeFirstCat() {
+    var peanut= cats.slice(1)
+    return peanut;
+  }
